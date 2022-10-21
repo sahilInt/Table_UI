802647eb6db667d429297440c691a2233eb98c53
sahilInt <sahil@intemize.com>
2022-10-21 12:03:22 +0530

    Dropdown Right SIdebar In Progress

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,286 @@
+// import React from 'react'
+
+// import "./Dropdown.css"
+// const { useState, useEffect } = React;
+
+// const data = [{ id: 0, label: "Istanbul, TR (AHL)" }, { id: 1, label: "Paris, FR (CDG)" }];
+
+// const Dropdown = () => {
+//     const [isOpen, setOpen] = useState(false);
+//     const [items, setItem] = useState(data);
+//     const [selectedItem, setSelectedItem] = useState(null);
+
+//     const toggleDropdown = () => setOpen(!isOpen);
+
+//     const handleItemClick = (id) => {
+//         selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);
+//     }
+
+//     return (
+//         <div className='dropdown'>
+//             <div className='dropdown-header' onClick={toggleDropdown}>
+//                 {selectedItem ? items.find(item => item.id == selectedItem).label : "Select your destination"}
+//             </div>
+//             <div className={`dropdown-body ${isOpen && 'open'}`}>
+//                 {items.map(item => (
+//                     <div className="dropdown-item" onClick={e => handleItemClick(e.target.id)} id={item.id}>
+//                         <span className={`dropdown-item-dot ${item.id == selectedItem && 'selected'}`}>• </span>
+//                         {item.label}
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
+// export default Dropdown;
+
+
+import "./Dropdown.css"
+
+import React, { useState, useEffect } from 'react'
+import Hexagone from "../../SVG/hexagonAqua.svg"
+import DownArrow from "../../SVG/downArrow.svg"
+import Circle from "../../SVG/circle.svg"
+import FilledCircle from "../../SVG/filledCircle.svg"
+import DropdownDownArrow from "../../SVG/download-one.svg"
+import PlaneHexagon from "../../SVG/hexagonopen.svg"
+import "../../Container/PersonalPatientFileMiddleScreen/PersonalPatientFileMiddleScreen.css"
+
+
+const data = [{ id: 0, label: "Istanbul, TR (AHL)" }, { id: 1, label: "Paris, FR (CDG)" }];
+
+const dataone=()=>{
+    return(
+        <div>HI</div>
+    )
+}
+
+const PPRightSideBar = () => {
+
+
+    const [isOpen, setOpen] = useState(false);
+    const [items, setItem] = useState(data);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const toggleDropdown = () => setOpen(!isOpen);
+
+    // const handleItemClick = (id) => {
+    //     selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);
+    // }
+
+
+
+    return (
+
+        <>
+
+            <div className='dropdown'>
+                <div className='dropdown-header' onClick={toggleDropdown}>
+                    {selectedItem ? items.find(item => item.id == selectedItem).label : "Select your destination"}
+                    <i className={`fa fa-chevron-right icon ${isOpen && "open"}`}></i>
+                </div>
+                <div className={`dropdown-body ${isOpen && 'open'}`}>
+                    {items.map(item => (
+                        <div className="dropdown-item">
+                            <div className={`dropdown-item-dot $`}>
+                            <dataone/>
+                            </div>
+                            {item.label}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+
+            <div className='ppms-main-div-two'>
+                <div className='ppms-main-div-sub-div-two'>
+                    <button>Stages of Treatment</button>
+                </div>
+                <div>
+                    <div className='treatment-details-main-div-one'>
+                        <div className='hexa-div-in-treat-main'>
+                            <img src={Hexagone} alt='' className='hexagone-svg-main' />
+                        </div>
+                        <div className='header-name-in-treat'>
+                            <h3>Screening</h3>
+                        </div>
+                        <div>
+                            <img src={DropdownDownArrow} alt='' className='down-arrow-svg-in-treat-one' />
+                        </div>
+                    </div>
+
+                    <div className='treat-ment-sub-parts'>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Existing Medical Tests</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Additional Medical Tests</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p> Comorbity Screening</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Phenotype Screeing</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div>
+                    <div className='treatment-details-main-div-one'>
+                        <div className='hexa-div-in-treat'>
+                            <img src={PlaneHexagon} alt='' className='hexagone-svg' />
+                        </div>
+                        <div className='header-name-in-treat'>
+                            <h3>Assessment</h3>
+                        </div>
+                        <div>
+                            <img src={DropdownDownArrow} alt='' className='down-arrow-svg-in-treat-one' />
+                        </div>
+                    </div>
+
+                    <div className='treat-ment-sub-parts'>
+                        <div className='treat-sub-header-main-div'>
+                            <div>
+                                <img src={FilledCircle} alt='' className='filled-circle-svg-in-treat' />
+                            </div>
+                            <div className='treat-sub-header-font-tag'>
+                                <p>Existing Medical Tests</p>
+                            </div>
+                            <div>
+                                <img src={DropdownDownArrow} alt='' className='down-arrow-svg-in-treat-one' />
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Medical History</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Child Growth</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p> Parental Obesity</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className='treat-ment-sub-parts'>
+                        <div className='treat-sub-header-main-div'>
+                            <div>
+                                <img src={FilledCircle} alt='' className='filled-circle-svg-in-treat' />
+                            </div>
+                            <div className='treat-sub-header-font-tag'>
+                                <p>Behaviour Risk</p>
+                            </div>
+                            <div>
+                                <img src={DropdownDownArrow} alt='' className='down-arrow-svg-in-treat-one' />
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Sedentary Time</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Eating</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p> Physical Activity</p>
+                            </div>
+                        </div>
+                        <div className='treat-sub-header-main-div'>
+                            <div>
+                                <img src={FilledCircle} alt='' className='filled-circle-svg-in-treat' />
+                            </div>
+                            <div className='treat-sub-header-font-tag'>
+                                <p>Activity</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='treatment-details-main-div-one'>
+                        <div className='hexa-div-in-treat'>
+                            <img src={PlaneHexagon} alt='' className='hexagone-svg' />
+                        </div>
+                        <div className='header-name-in-treat'>
+                            <h3>Intervention</h3>
+                        </div>
+                        <div>
+                            <img src={DropdownDownArrow} alt='' className='down-arrow-svg-in-treat-one' />
+                        </div>
+                    </div>
+
+                    <div className='treat-ment-sub-parts'>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Surgical</p>
+                            </div>
+                        </div>
+                        <div className='treatment-details-main-div-one-sub-one'>
+                            <div>
+                                <img src={Circle} alt='' className='down-arrow-svg-in-treat' />
+                            </div>
+                            <div className='header-name-in-treat-sub-sec'>
+                                <p>Medical</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PPRightSideBar
---
+++ src/Components/Dropdown/drop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const drop = () => {
+  return (
+    <div>drop</div>
+  )
+}
+
+export default drop
